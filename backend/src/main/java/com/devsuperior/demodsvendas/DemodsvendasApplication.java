419335ba89455f53fd0b033b78b9531c8493fa71
igor-lourenco <igor.lourencosantos@gmail.com>
2021-05-03 14:25:42 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/demodsvendas/DemodsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demodsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/demodsvendas/DemodsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demodsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
